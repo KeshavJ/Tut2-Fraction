4e74d62d9a7e8295c8548021d86d42a6472c9698
KeshavJ <keshav.jeewanlall@gmail.com>
2016-03-03 14:10:25 +0200

    added subtraction, multiplication and division to code

--- FractionTut2/Fraction.cpp
+++ FractionTut2/Fraction.cpp
@@ -8,7 +8,7 @@ private:
 	int denominator;
 
 public:
-													// Declaration of all methods
+									// Declaration of all methods
 	void setNumDenom(int, int);
 	Fraction getNumDenom();
 	Fraction add(Fraction);
@@ -17,11 +17,11 @@ public:
 	Fraction divide(Fraction);
 	void print();
 	int hcf(int, int);
-	Fraction operator+(Fraction &);  // allows f1+f2
-	Fraction operator-(Fraction &); // allows f1-f2
+	Fraction operator+(Fraction &);	// allows addition of objects fract1+fract2
+	Fraction operator-(Fraction &);	// allows subtraction of objects fract1-fract2
 	Fraction();
-	Fraction(int, int);								//Constructor declaration
-	~Fraction();									//Destructor declaration
+	Fraction(int, int);	//Constructor declaration
+	~Fraction();	//Destructor declaration
 };
 
 Fraction::Fraction() {
@@ -32,17 +32,17 @@ Fraction::Fraction() {
 Fraction::~Fraction() {
 }
 
-Fraction::Fraction(int n, int d) {					//Default constructor
+Fraction::Fraction(int n, int d) {	//constructor with parameters
 	numerator = n;
 	denominator = d;
 }
 
-void Fraction::setNumDenom(int num, int den) {		//Mutator function
+void Fraction::setNumDenom(int num, int den) {	//Mutator function
 	numerator = num;
 	denominator = den;
 }
 
-Fraction Fraction::getNumDenom() {					//Accessor function
+Fraction Fraction::getNumDenom() {	//Accessor function
 	return Fraction(numerator, denominator);
 }
 
@@ -63,7 +63,7 @@ Fraction Fraction::divide(Fraction f) {
 }
 
 int Fraction::hcf(int n, int d) {
-	if (d == 0){
+	if (d == 0) {
 		return n;
 	}
 	else {
@@ -76,30 +76,31 @@ void Fraction::print() {
 	int i = 0;
 	int a = numerator;
 	int b = denominator;
-	int rem = a%b;									//gets remainder
-	int hcf1 = hcf(a, b);							//hcf for numerator & denominator
-	int hcf2 = hcf(rem, b);							//hcf for remainder and denominator
+	int rem = a % b;	//gets remainder
+	int hcf1 = hcf(a, b);	//hcf for numerator & denominator
+	int hcf2 = hcf(rem, b);		//hcf for remainder and denominator
 
 
-	if (a > b)			// counts how many times the denominator goes to te numerator if it is an improper fraction
+	if (a > b)	// counts how many times the denominator goes to te numerator if it is an improper fraction
 	{
-		
-			do
-			{
-				a -= b;
-				i++;
-			} while (a > 0);
-		}
 
-		if ((i) == 0)
+		do
 		{
+			a -= b;
+			i++;
+		} while (a >= 0);
+	}
 
-			cout << a/hcf1 << "/" << b/hcf1;
-		}
-		else {
+	if ((i) == 0)
+	{
 
-			cout << i-1 << " " << rem / hcf2 << "/" << b / hcf2;
-		}
+		cout << a / hcf1 << "/" << b / hcf1;
+	}
+	else {
+
+		cout << i-1 << " " << rem / hcf2 << "/" << b / hcf2;
+	}
+
 	}
 
 
@@ -125,9 +126,31 @@ int main() {
 	fract2.print();
 	cout << " = ";
 
-	//overloading the + operator which allows addition
 	fract3 = fract1 + fract2;
+	fract3.print();
+	cout << endl;	
+
+	fract1.print();
+	cout << " - ";
+	fract2.print();
+	cout << " = ";
+	fract3 = fract1 - fract2;
 	fract3.print();
 	cout << endl;
 
+	fract1.print();
+	cout << " * ";
+	fract2.print();
+	cout << " = ";
+	fract3 = fract1.multiply(fract2);
+	fract3.print();
+	cout << endl;
+
+	fract1.print();
+	cout << " / ";
+	fract2.print();
+	cout << " = ";
+	fract3 = fract1.divide(fract2);
+	fract3.print();
+	cout << endl;
 }
